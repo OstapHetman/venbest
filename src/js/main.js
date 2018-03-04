1be3d99c31ff5a3b45292bd04e431e43d275268c
OstapHetman <prickly4528@gmail.com>
2018-03-04 13:39:07 +0200

    implement scroll top btn

--- src/js/main.js
+++ src/js/main.js
@@ -17,3 +17,12 @@ $('#protectCarousel').on('slide.bs.carousel', function(e) {
         }
     }
 });
+// Scroll Top
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+        $('.scrolltop:hidden').stop(true, true).fadeIn();
+    } else {
+        $('.scrolltop').stop(true, true).fadeOut();
+    }
+});
+$(function() { $(".scroll").click(function() { $("html,body").animate({ scrollTop: $("#top-part").offset().top }, "1000"); return false }) })
